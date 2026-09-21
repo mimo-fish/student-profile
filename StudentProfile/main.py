32a46181a464dcf54220033efa62c2382fcb2031
mimo-fish <spla11037toon@gmail.com>
2026-09-22 01:01:20 +0300

    Initial commit

---
+++ StudentProfile/main.py
@@ -0,0 +1,10 @@
+name = "Lamees Aljaroof" 
+major = "Artificial Intelligence"
+university = "Immam Abdulrahman Bin Faisal University" 
+hobby = "Reading"
+print("Student Profile")
+print("----------------")
+print("Name:", name) 
+print("Major:", major)
+print("University:", university)
+print("Hobby:", hobby)
